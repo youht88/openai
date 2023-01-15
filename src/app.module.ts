d2e368b640dd5c43b062d64da5445058c623a379
youht88 <1742716458@qq.com>
2023-01-15 14:13:34 +0800

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatgptModule } from './openai/chatgpt/chatgpt.module';
 import { OpenaiModule } from './openai/openai/openai.module';
 
 @Module({
-  imports: [OpenaiModule, ChatgptModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['./env/dev.env'],
+    }),
+    OpenaiModule,
+    ChatgptModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
