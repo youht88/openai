13b4cf47ef2731ecbcbf903cc8f8974630e180cf
youht88 <1742716458@qq.com>
2023-01-18 08:27:58 +0800

    test html

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Render } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get("/hello")
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get("/test")
+  @Render("index.html")
+  root() {
+    return { message: "just a test" };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { AppService } from './app.service';
+import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { AppModule } from "./app.module";
+import { AppService } from "./app.service";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(3000);
   const appService = app.get<AppService>(AppService);
   appService.start();
