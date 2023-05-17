196ee4f93f412c22a573f93b1094fdb2b18d2eee
youht88 <1742716458@qq.com>
2023-05-17 04:56:34 +0000

    update docker_deploy.s390.sh

--- docker_deploy.s390.sh
+++ docker_deploy.s390.sh
@@ -43,16 +43,18 @@ set -e
 set +e # 下面这行失败是可以接受的
 #docker rm -f ${businessName}
 docker service rm ${businessName}
-docker config rm ${businessName}
+docker config rm ${businessName}_config
+docker network create mynet -d overlay
 set -e
 
 # 运行容器
 echo "如果你想手动查找问题，可以试试以下命令来运行容器"
 echo "docker run -it --rm --name ${businessName} -v env:/home/env -p 8088:3000  $imgName /bin/sh "
-docker config create ${businessName} ~/.env/.dev.yaml
+docker config create ${businessName}_config ~/.env/.dev_0517.yaml
 docker service create --name ${businessName} \
             --replicas 1 \
-            --config source=${businessName},target=/home/env/.dev.yaml \
+	    --network mynet \
+            --config source=${businessName}_config,target=/home/env/.dev.yaml \
             -p 9322:3000 \
             $imgName
 
